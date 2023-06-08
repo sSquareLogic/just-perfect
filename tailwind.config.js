851596719bfdc25712e6c476a7275b621af8d7f1
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-06-09 00:23:18 +0500

    assets & variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        GREEN: "#BEFA1B",
+        BLACK: "#2D2D2D",
+        GRAY: "#6A6A6A",
+        WHITE: "#FFFFFF",
+        BG: "#F6F6F6",
+      },
+      fontSize: {
+        TITLE: "40px",
+        SM_TITLE: "32px",
+        LG_TEXT: "24px",
+        TEXT: "18px",
+        SM_TEXT: "16px",
+        XS_TEXT: "14px",
       },
     },
   },
   plugins: [],
-}
+};

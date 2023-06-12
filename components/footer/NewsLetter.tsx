2ba63bb9cbee0740711f9de7cfeb58e6f80df67a
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-06-12 23:17:16 +0500

    div -> form

--- components/footer/NewsLetter.tsx
+++ components/footer/NewsLetter.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { FormEvent } from "react";
 import NewsLetterInput from "./NewsLetterInput";
 
 const NewsLetter = () => {
   return (
-    <div className="newsletter flex flex-col gap-2 text-WHITE font-GR text-XS_TEXT">
+    <form
+      className="newsletter flex flex-col gap-2 text-WHITE font-GR text-XS_TEXT"
+      onSubmit={(e: FormEvent) => e.preventDefault()}
+    >
       <span>Получайте информацию о скидках и акциях</span>
       <NewsLetterInput />
       <span className="pt-2">
@@ -10,7 +15,7 @@ const NewsLetter = () => {
         данных в соотвествии с политикой по обработке персональных данных, а
         также соглошаетесь с условиями публичной оферты
       </span>
-    </div>
+    </form>
   );
 };
 

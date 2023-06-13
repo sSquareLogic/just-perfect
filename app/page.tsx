4a6c73676f8ed2564673072da2eacb7f6af20518
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-06-13 11:57:07 +0500

    clean testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Card from "@/components/card/Card";
 import Hero from "@/components/hero/Hero";
 
 const Home = () => {
   return (
     <main className="main">
       <Hero />
-      <div className="w-[312px]">
-        <Card img="/assets/placeholders/new-1.jpg" name="ASDF" price="300" />
-      </div>
     </main>
   );
 };

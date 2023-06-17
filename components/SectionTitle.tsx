84da807ba5a29baf0211980e74075c592fc683ae
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-06-17 18:45:49 +0500

    more abstact section title

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,10 +1,18 @@
 interface IProps {
   title: string;
+  align?: "left" | "right" | "center";
 }
 
-const SectionTitle = ({ title }: IProps) => {
+const SectionTitle = ({ title, align = "left" }: IProps) => {
   return (
-    <h2 className="section-title text-BLACK text-SM_TITLE font-RW font-bold">
+    <h2
+      className="section-title text-BLACK text-SM_TITLE font-RW font-bold"
+      style={
+        align === "center"
+          ? { width: "100%", textAlign: align }
+          : { textAlign: align }
+      }
+    >
       {title}
     </h2>
   );
